a0014b9eb1fbe9a2a2999e902f732a4b5c1ff4d6
Allex Lasse <allex_lasse@hotmail.com>
2019-02-06 19:56:48 -0200

    Classes estaticas - classe

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,9 @@
 package application;
 import java.util.Locale;
 import java.util.Scanner;
+import util.Calculator;
 
 public class Program {
-	
-	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		
@@ -14,19 +13,11 @@ public class Program {
 		System.out.print("Radius: ");
 		double radius = sc.nextDouble();
 		
-		System.out.println("Volume: " + String.format("%.2f", Volume(radius))
-				+"\nCircumference: " + String.format("%.2f", Circumference(radius))
-				+ "\nPi value: " + PI);
+		System.out.println("Volume: " + String.format("%.2f", Calculator.Volume(radius))
+				+"\nCircumference: " + String.format("%.2f", Calculator.Circumference(radius))
+				+ "\nPi value: " + Calculator.PI);
 		
 		sc.close();
-	}
-	
-	public static double Circumference(double radius) {
-		return 2 * PI * radius;
-	}
-	
-	public static double Volume(double radius) {
-		return (4.0 * PI * Math.pow(radius, 3)) / 3.0;
 	}
 
 }
